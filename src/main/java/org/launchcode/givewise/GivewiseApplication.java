ad3cf7890c9f45fc59fa121751b858cd90b7028b
HARIPRIYA92 <haripriyamnair1992@gmail.com>
2024-01-16 18:43:11 -0600

    Initial Spring boot Application Setup

---
+++ src/main/java/org/launchcode/givewise/GivewiseApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.givewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivewiseApplication.class, args);
+	}
+
+}
