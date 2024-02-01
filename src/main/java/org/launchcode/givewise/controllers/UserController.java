5483a83670941a0c9386f802e10e0e273a4aa60b
HARIPRIYA92 <haripriyamnair1992@gmail.com>
2024-01-31 19:41:00 -0600

    Changes on user controller
    Changes on user controller to correct issue related to json undefined error

--- src/main/java/org/launchcode/givewise/controllers/UserController.java
+++ src/main/java/org/launchcode/givewise/controllers/UserController.java
@@ -1,28 +1,30 @@
 package org.launchcode.givewise.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.launchcode.givewise.models.User;
 import org.launchcode.givewise.request.UserRequest;
 import org.launchcode.givewise.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
     @PostMapping("/register")
-    public ModelAndView registerUser(@RequestBody UserRequest userDto) {
-        ModelAndView response = new ModelAndView();
-        // You can validate and process the data here
-        // For simplicity, let's assume there's a UserService to handle user registration
-        User user=userService.registerUser(userDto);
-        response.addObject("user", user);
-        return response;
+    public ResponseEntity<User> registerUser(@RequestBody UserRequest userDto) {
+            // Validate and process the data
+            User user = userService.registerUser(userDto);
+
+        log.info("User created: {}", user);
+            // Check if user registration was successful
+                return ResponseEntity.ok(user);
     }
 }
--- src/main/java/org/launchcode/givewise/models/data/UserRepository.java
+++ src/main/java/org/launchcode/givewise/models/data/UserRepository.java
@@ -2,7 +2,10 @@ package org.launchcode.givewise.models.data;
 
 import org.launchcode.givewise.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByEmailIgnoreCase(String email);
 }
