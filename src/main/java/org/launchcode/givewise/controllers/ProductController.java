d361e26a7eb2f6329097ee24e447c79b6d3825b4
Sam <ssmarie14@yahoo.com>
2024-02-19 21:40:54 -0600

    added search bar functionality working on backend

--- src/main/java/org/launchcode/givewise/controllers/ProductController.java
+++ src/main/java/org/launchcode/givewise/controllers/ProductController.java
@@ -6,9 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.launchcode.givewise.models.Product;
 import org.launchcode.givewise.models.data.ProductRepository;
 import org.launchcode.givewise.request.ProductRequest;
-
 import org.launchcode.givewise.service.MailjetService;
-
 import org.launchcode.givewise.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,36 +23,43 @@ public class ProductController {
 
     @Autowired
     private MailjetService mailjetService;
+
     @Autowired
     private ProductService productService;
 
     @Autowired
     private ProductRepository productRepository;
+
     @Operation(security = {@SecurityRequirement(name = BASIC_AUTH_SECURITY_SCHEME)})
     @PostMapping("/add")
-    public ResponseEntity<Product> addProduct(@RequestBody ProductRequest productDto){
+    public ResponseEntity<Product> addProduct(@RequestBody ProductRequest productDto) {
         Product product = productService.addProduct(productDto);
         log.info("Product added: {}", product);
-      //  mailjetService.notifySubscriberOnNewProduct(product);
         return ResponseEntity.ok(product);
     }
-    @GetMapping("/list")
-    public ResponseEntity<List<Product>> getProduct(){
-        List<Product> products = productRepository.getAllProducts();
 
+    @GetMapping("/list")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productRepository.getAllProducts();
         return ResponseEntity.ok(products);
-    };
+    }
+
     @Operation(security = {@SecurityRequirement(name = BASIC_AUTH_SECURITY_SCHEME)})
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Integer id){
-        try{
+    public ResponseEntity<Void> deleteProduct(@PathVariable Integer id) {
+        try {
             productService.deleteProduct(id);
             log.info("Product deleted with id: {}", id);
             return ResponseEntity.noContent().build();
-        }catch (Exception e) {
+        } catch (Exception e) {
             log.error("Error deleting product with id: {}", id, e);
             return ResponseEntity.status(500).build();
         }
     }
-}
 
+    @GetMapping("/search/{searchTerm}")
+    public ResponseEntity<List<Product>> searchProductsByCharacter(@PathVariable String searchTerm) {
+        List<Product> products = productService.searchProductsByCharacter(searchTerm);
+        return ResponseEntity.ok(products);
+    }
+}
